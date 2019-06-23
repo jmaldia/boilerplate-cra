cf7a8615258074844573d3ed06413c215fc57c1b
Jon Maldia <jon@maldia.net>
2019-06-23 19:41:09 -0400

    Add custom hooks

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
 // CUSTOM FILES
 import NotesContext from '../context/notes-context';
-
-
+import useMousePosition from '../hooks/useMousePosition';
 
 const Note = ({ title, body, removeNote }) => {
     const { dispatch } = useContext(NotesContext);
+    const position = useMousePosition();
     
     return (
         <div>
             <h3>{ title }</h3> 
             <p>{ body }</p> 
+            <p>{ position.x }, { position.y }</p>
             <button onClick={ () => dispatch({
                 type: 'REMOVE_NOTE',
                 title
